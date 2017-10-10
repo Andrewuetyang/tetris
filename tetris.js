dc9bd9790e57a26bc3a3ddf26ea231a4bca0b622
Andrew Yang <andrewyang@AndrewdeMacBook-Pro.local>
2017-10-10 22:22:44 +0800

    优化

--- tetris.js
+++ tetris.js
@@ -275,14 +275,3 @@ Tetris.prototype = {
         return arr
     }
 }
-
-var tetrisGame = new Tetris({
-    row: 12,
-    col: 12,
-    grid: 30,
-    margin: 10,
-    offsetX: 4,
-    interval: 400,
-    fasterInterval: 100
-})
-tetrisGame.start()
